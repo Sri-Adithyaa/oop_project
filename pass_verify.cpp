e9bdff45e7f71d731f9f81ecd4ea1bf8c3bb2761
Sri-Adityaa <sriadithyaa.m@gmail.com>
2026-03-18 15:24:51 +0530

    updated the login file

--- pass_verify.cpp
+++ pass_verify.cpp
@@ -32,6 +32,7 @@ class User
                 return *this;
             }
             friend istream& operator>>(istream &is,User &u1);
+            friend ostream& operator<<(ostream &os,User &u1);
             bool validate(string user,string key)
             {
                 if(name==user && pass == key)
@@ -52,18 +53,25 @@ istream& operator>>(istream &is,User &u1)
     is>>u1.pass;
     return is;
 }
+ostream& operator<<(ostream& os,User &u1)
+{
+    os<<"\n"<<u1.name<<","<<u1.pass;
+    return os;
+}
 int main()
 {
     string user,key;
     User user1;
-    cout<<"starting he engine...";
+    cout<<"starting the engine...\n";
     for(int i=0;i<3;i++)
     {
     cout<<"\rstart's in "<<i+1<<flush;
     this_thread::sleep_for(chrono::seconds(1));
     }
     int ch;
-    l1:cout<<"1) to register a new user"<<endl;
+    while(true)
+    {
+    cout<<"\n1) to register a new user"<<endl;
     cout<<"2) login"<<endl;
     cout<<"enter your choice= ";
     cin>>ch;
@@ -71,6 +79,37 @@ int main()
     switch(ch)
     {
         case 1:
+        {
+            cin>>user1;
+            int found=1;
+            fstream file1("user.txt",ios::in);
+            while(getline(file1,line))
+            {
+                stringstream str(line);
+                getline(str,user,',');
+                getline(str,key,',');
+                if(user1.validate(user,key))
+                {
+                    cout<<"user already excist"<<endl;
+                    found=0;
+                    break;
+                }
+            }
+            file1.close();
+            if(found)
+            {
+                fstream file("user.txt",ios::app);
+                file<<user1;
+                cout<<"\nuser is being added...\n"<<endl;
+                for(int i=0;i<3;i++)
+                {
+                cout<<"\rtake's few seconds "<<i+1<<flush;
+                this_thread::sleep_for(chrono::seconds(1));
+                }
+                return 1;//returning 1 to for adding new user
+            }
+        }
+        case 2:
         {
             cin>>user1;
             fstream file1("user.txt",ios::in);
@@ -81,23 +120,12 @@ int main()
                 getline(str,key,',');
                 if(user1.validate(user,key))
                 {
-                    cout<<"user already excist"<<endl;
-                    goto l1;
+                    break;
                 }
             }
             file1.close();
-            fstream file("user.txt",ios::app);
-            file<<"\n"<<user<<','<<key<<endl;
-            cout<<"user is being added..."<<endl;
-            for(int i=0;i<3;i++)
-            {
-            cout<<"\rtake's few seconds "<<i+1<<flush;
-            this_thread::sleep_for(chrono::seconds(1));
-            }
-        }
-    }
-
-    
-
-
-}
+            return 2;//returniing 2 for suceesfully addeing the user
+        }//closing case2
+    }//closing switch
+    }//closing while
+}//closing main
